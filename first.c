7d24d2d955f5ffae8add9e9f98d613678dc2fd1f
Berik468 <berkeyman468@gmail.com>
2022-03-24 18:18:27 +0300

    git push origin master

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int age;
+    printf("Enter some number bitch:  \n");
+    scanf("%d", &age);
+    printf("You are %d years old? mf you old. die already \n", age);
+
+    return 0;
+}
